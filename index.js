e6f3d9834869ea06c6f03b99cfee42624b0f6483
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-01-06 21:08:18 +0100

    update
    All works!!!

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
+// @ts-nocheck
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
 } = require("./contacts");
+const { Command } = require('commander');
+const program = new Command();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
-    case "getAll":
+    case "list":
       const allContacts = await listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
-    case "getById":
+    case "get":
       const choosenContact = await getContactById(id);
       return console.log(choosenContact);
 
-    case "delete":
+    case "remove":
       const contactDelete = await removeContact(id);
       return console.log(contactDelete);
 
     case "add": 
     const newContact = await addContact({name, email, phone});
     return console.log(newContact);
+
+    default:
+        console.warn('\x1B[31m Unknown action type!');
   }
 };
 
-// @ts-ignore
-// invokeAction({ action: "getAll" });
-// invokeAction({action: "getById", id: "drsAJ4SHPYqZeG-83QTVW"});
-// invokeAction({action: "delete", id: "vza2RIzNGIwutCVCs4mCL"});
-// invokeAction({action: 'add', name: 'Vladylsav', email: 'frederiko@gmail.com', phone: '+34678987098'});
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+invokeAction(argv);
+
+// # Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+// node index.js --action="list"
+
+// # Отримуємо контакт по id і виводимо у консоль об'єкт контакту або null, якщо контакту з таким id не існує.
+// node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+
+// # Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+// node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+
+// # Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null, якщо контакту з таким id не існує.
+// node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
